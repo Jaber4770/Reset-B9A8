08ac0311d1d6f5bb3a8c0a959c03116a764593d7
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2024-07-02 23:40:27 +0300

    working

--- src/Component/ListedBooks/ListedBooks.jsx
+++ src/Component/ListedBooks/ListedBooks.jsx
@@ -1,10 +1,23 @@
 import { FaChevronDown } from "react-icons/fa";
-import { Link, Outlet } from "react-router-dom";
-
+import { Link, Outlet, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const ListedBooks = () => {
+    const location = useLocation();
 
+    const [activeButton, setActiveButton] = useState("");
 
+    useEffect(() => {
+        if (location.pathname.includes("readinglist")) {
+            setActiveButton("readinglist");
+        } else if (location.pathname.includes("wishlist")) {
+            setActiveButton("wishlist");
+        }
+    }, [location.pathname]);
+
+    const handleButtonClick = (button) => {
+        setActiveButton(button);
+    };
 
     return (
         <div className="bg-white text-black">
@@ -14,8 +27,8 @@ const ListedBooks = () => {
                 </div>
                 <div className="py-10">
                     <details className="dropdown">
-                        <summary className="btn m-1 px-7 flex items-center gap-5 bg-[#23BE0A] border-none text-white font-bold hover:bg-[#198a07]">Sort By <FaChevronDown /></summary>
-                        <ul className="menu dropdown-content rounded-box z-[1] w-52 p-2 shadow-lg">
+                        <summary className="btn m-1 px-14 flex items-center gap-5 bg-[#23BE0A] border-none text-white font-bold hover:bg-[#198a07]">Sort By <FaChevronDown /></summary>
+                        <ul className="menu dropdown-content bg-gray-200 rounded-box z-[1] w-52 p-2 shadow-lg">
                             <li><a>Rating</a></li>
                             <li><a>Published year</a></li>
                             <li><a>Number of pages</a></li>
@@ -25,15 +38,23 @@ const ListedBooks = () => {
                 <div>
                     <div className="border-b-[2px] flex gap-4">
                         <Link to="/listedbooks/readinglist">
-                           Read Books
+                            <button
+                                onClick={() => handleButtonClick("readinglist")}
+                                className={`${activeButton === "readinglist" ? "border-b-2 border-black" : ""}`}
+                            >
+                                Read Books
+                            </button>
                         </Link>
                         <Link to="/listedbooks/wishlist">
-                           Wish List
+                            <button
+                                onClick={() => handleButtonClick("wishlist")}
+                                className={`${activeButton === "wishlist" ? "border-b-2 border-black" : ""}`}
+                            >
+                                Wish List
+                            </button>
                         </Link>
                     </div>
-                    <Outlet></Outlet>
-                    <div>
-                    </div>
+                    <Outlet />
                 </div>
             </div>
         </div>
